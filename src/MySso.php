16da4fcee6c25fa89a4b5efe0e0a20b837035012
wangjingchun <wangjingchun@ebrun.com>
2016-10-27 10:26:03 +0800

    修改MySso.php

--- src/MySso.php
+++ src/MySso.php
@@ -10,15 +10,32 @@ class MySso
 	public $broker;
 	public $secret;
 
-	public function __construct($url, $broker, $secret)
+	public function __construct($server_url = null, $broker = null, $secret = null)
 	{
-		$this->url = $url;
+		if (is_null($server_url)) {
+			throw new \InvalidArgumentException("server url not specified");
+		}
+		if (is_null($broker)) {
+			throw new \InvalidArgumentException("broker not specified");
+		}
+		if (is_null($secret)) {
+			throw new \InvalidArgumentException("secret not specified");
+		}
+
+		$this->url = $server_url;
         $this->broker = $broker;
         $this->secret = $secret;
 	}
 
-	public function login($username, $password)
+	public function login($username = null, $password = null)
 	{
+		if (is_null($username)) {
+			throw new \InvalidArgumentException("username not specified");
+		}
+		if (is_null($password)) {
+			throw new \InvalidArgumentException("password not specified");
+		}
+
 		$broker = new Broker($this->url, $this->broker, $this->secret);
 		$broker->attach(true);
 
